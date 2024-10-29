d3a23f66523bb827628c819077f5a001e4fdede9
Ericksonxd165 <ericksonxd_164@hotmail.com>
2024-10-29 11:09:43 -0400

    arreglar luego

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const d =document;
+
+import toggler from "./js/toggler.js";
+
+
+d.addEventListener("DOMContentLoaded",(e)=>{
+
+toggler("#login","#register","#buttonl")
+
+
+
+
+})
---
+++ js/toggler.js
@@ -0,0 +1,15 @@
+const d =document;
+
+export default function toggler(element1,element2,buton){
+
+d.addEventListener("click",e=>{
+
+if(e.target.matches(buton)){
+
+d.getElementById(element1).classList.toggle('invisible')
+console.log("hola")
+}
+
+
+})
+ }
