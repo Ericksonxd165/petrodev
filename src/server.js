2cff4fa17a5f993ba52b4030bc511383f0be7847
Erick Pereira <ericksonxd_164@hotmail.com>
2025-01-24 18:49:52 -0400

    sesion de super usuario

--- src/server.js
+++ src/server.js
@@ -38,6 +38,114 @@ app.use(session({
   cookie: { secure: false } // Cambia a true si usas HTTPS
 }));
 
+
+
+// Ruta para manejar el login de administrador
+app.post('/adminLogin', (req, res) => {
+  const { email, password } = req.body;
+
+  const sql = 'SELECT * FROM admin WHERE email = ?';
+  db.query(sql, [email], async (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      const user = results[0];
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        req.session.user = user;
+        req.session.isAdmin = true; // Marcar la sesión como de administrador
+        return res.status(200).json({ message: 'Inicio de sesión exitoso', redirect: '/admindashboard.html' });
+      } else {
+        return res.status(400).json({ message: 'Contraseña incorrecta' });
+      }
+    } else {
+      return res.status(400).json({ message: 'Usuario no encontrado' });
+    }
+  });
+});
+
+// Middleware para verificar si el usuario es estudiante
+function isEstudiante(req, res, next) {
+  if (req.session.user && !req.session.isAdmin && req.session.user.rol === 'estudiante') {
+    return next();
+  } else {
+    return res.redirect('/index.html');
+  }
+}
+
+
+// Middleware para verificar si el usuario es administrador
+function isAdmin(req, res, next) {
+  if (req.session.user && req.session.isAdmin) {
+    return next();
+  } else {
+    return res.redirect('/index.html');
+  }
+}
+// Rutas protegidas para administradores
+app.get('/admindashboard.html', isAdmin, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'admindashboard.html'));
+});
+
+app.get('/adminstudents.html', isAdmin, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'adminstudents.html'));
+});
+
+app.get('/admintasks.html', isAdmin, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'admintasks.html'));
+});
+
+// Rutas protegidas para profesores
+app.get('/dashboard.html', isProfesor, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+app.get('/library.html', isProfesor, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+app.get('/capacitacion.html', isProfesor, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+app.get('/editor.html', isProfesor, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+app.get('/proyectos.html', isProfesor, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+app.get('/modulos.html', isProfesor, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+app.get('/tareas.html', isProfesor, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'dashboard.html'));
+});
+
+// Rutas protegidas para estudiantes
+app.get('/studentdashboard.html', isEstudiante, (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'studentdashboard.html'));
+});
+
+// Middleware para verificar si el usuario es profesor
+function isProfesor(req, res, next) {
+  if (req.session.user && !req.session.isAdmin && req.session.user.rol === 'profesor') {
+    return next();
+  } else {
+    return res.redirect('/index.html');
+  }
+}
+
+// Redirigir a admindashboard si el administrador ya está autenticado
+app.get(['/adminLogin'], (req, res) => {
+  if (req.session.user && req.session.isAdmin) {
+    res.redirect('/admindashboard.html');
+  } else {
+    res.sendFile(path.join(__dirname, 'html', req.path));
+  }
+});
+
 function isAuthenticated(req, res, next) {
   if (req.session.user) {
     return next();
@@ -45,6 +153,32 @@ function isAuthenticated(req, res, next) {
     return res.status(401).json({ message: 'Usuario no autenticado' });
   }
 }
+
+// Redirigir a dashboard si el profesor ya está autenticado
+app.get(['/login'], (req, res) => {
+  if (req.session.user && req.session.user.rol === 'profesor') {
+    res.redirect('/admindashboard.html');
+  } else {
+    res.sendFile(path.join(__dirname, 'html', req.path));
+  }
+});
+
+
+// Ruta para manejar la redirección a index.html y cerrar sesión de administrador
+app.get('/index.html', (req, res) => {
+  if (req.session.isAdmin) {
+    req.session.destroy((err) => {
+      if (err) {
+        return res.status(500).send('Error al cerrar sesión');
+      }
+      res.clearCookie('connect.sid');
+      res.sendFile(path.join(__dirname, 'html', 'index.html'));
+    });
+  } else {
+    res.sendFile(path.join(__dirname, 'html', 'index.html'));
+  }
+});
+
 
 // Ruta para manejar el inicio de sesión
 app.post('/login', (req, res) => {
@@ -65,6 +199,17 @@ app.post('/login', (req, res) => {
     } else {
       return res.status(400).json({ message: 'Usuario no encontrado' });
     }
+  });
+});
+
+// Ruta para cerrar sesión
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Error al cerrar sesión' });
+    }
+    res.clearCookie('connect.sid');
+    return res.status(200).json({ message: 'Sesión cerrada exitosamente' });
   });
 });
 
